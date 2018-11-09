a822f354e904db18ffd04c5f3be0fcfd740ba0f0
jeffreybarry <10522393+jeffreybarry@users.noreply.github.com>
2018-11-09 13:13:57 -0500

    updated footer.php

--- footer.php
+++ footer.php
@@ -15,20 +15,12 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'custom-theme-for-the-shenandoah-literary-magazine' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'custom-theme-for-the-shenandoah-literary-magazine' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'custom-theme-for-the-shenandoah-literary-magazine' ), 'custom-theme-for-the-shenandoah-literary-magazine', '<a href="http://jeffbarry.org">Jeff Barry</a>' );
-				?>
+
+
+
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
-</div><!-- #page -->
+</div><!-- #page  also ends Bootstrap container-->
 
 <?php wp_footer(); ?>
 
