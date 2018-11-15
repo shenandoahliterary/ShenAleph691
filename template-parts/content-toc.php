571a2e81c056926237d866658926c6c83c7a982c
jeffreybarry <10522393+jeffreybarry@users.noreply.github.com>
2018-11-15 10:13:41 -0500

    updating toc

--- template-parts/content-toc.php
+++ template-parts/content-toc.php
@@ -19,56 +19,52 @@
 
 		<h3>Fiction</h3>
 		<div>
-			<ul>
-			<?php 	$fiction_loop = new WP_Query('cat=3&orderby=meta_value&meta_key=author_lastname&order=asc&nopaging=true');
+			<?php 	$fiction_loop = new WP_Query('cat=4&orderby=meta_value&meta_key=author_lastname&order=asc&nopaging=true');
 					while ($fiction_loop->have_posts()) : $fiction_loop->the_post();
 					 ?>
-					<li><span class="author_name"><?php the_author(); ?></span><br />
-					<a href="<?php the_permalink(); ?>">
-					<?php the_title(); ?>
-					</a></li>
+					<p>	<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a><br />
+
+						<span class="author_name"><?php the_author(); ?></span>
+
+				</p>
 			<?php endwhile; ?>
-			</ul>
 		</div>
 		<h3>Nonfiction</h3>
 		<div>
 			<?php $nonfiction_loop = new WP_Query('cat=5&orderby=meta_value&meta_key=author_lastname&order=asc&nopaging=true');
 					while ($nonfiction_loop->have_posts()) : $nonfiction_loop->the_post();
 					 ?>
+					 <p>
 					 <a href="<?php the_permalink(); ?>">
  					<?php the_title(); ?>
  					</a><br />
 					<span class="author_name"><?php the_author(); ?> </span>
-
+</p>
 			<?php endwhile; ?>
 		</div>
 
 		<h3>Interviews</h3>
 		    			<div>
-		    				<ul>
 		    				<?php 	$reviews_loop = new WP_Query('cat=6&orderby=meta_value&meta_key=author_lastname&order=asc&nopaging=true');
 		    						while ($reviews_loop->have_posts()) : $reviews_loop->the_post();
 		    						 ?>
-		    						<li><span class="author_name"><?php the_author(); ?> </span><br />
-		    						<a href="<?php the_permalink(); ?>">
-		    						<?php the_title(); ?>
-		    						</a></li>
+										 <a href="<?php the_permalink(); ?>">
+ 		    						<?php the_title(); ?>
+ 		    						</a><br />
+		    						<span class="author_name"><?php the_author(); ?> </span>
 		    				<?php endwhile; ?>
-		    				</ul>
 		    			</div>
 
 							<h3>Comics</h3>
 			    			<div>
-			    				<ul>
-			    				<?php 	$visualarts_loop = new WP_Query('cat=6&orderby=meta_value&meta_key=author_lastname&order=asc&nopaging=true');
+			    				<?php 	$visualarts_loop = new WP_Query('cat=7&orderby=meta_value&meta_key=author_lastname&order=asc&nopaging=true');
 			    						while ($visualarts_loop->have_posts()) : $visualarts_loop->the_post();
 			    						 ?>
-			    						<li><span class="author_name"><?php the_author(); ?> </span><br />
+			    						<span class="author_name"><?php the_author(); ?> </span><br />
 			    						<a href="<?php the_permalink(); ?>">
 			    						<?php the_title(); ?>
-			    						</a></li>
+			    						</a>
 			    				<?php endwhile; ?>
-			    				</ul>
 			    			</div>
 
 
@@ -100,7 +96,7 @@
 				<?php
 				$poetry_loop_single = new WP_Query($args);
 				//the following returns an error in php 7.2
-				//not clear that the count is needed 
+				//not clear that the count is needed
 				//$numItems = count($poetry_loop_single);
 				$i = 0;
 					while ($poetry_loop_single->have_posts()) : 				$poetry_loop_single->the_post();
