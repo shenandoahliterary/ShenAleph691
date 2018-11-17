7b26d6b7054dfaa1a0a3ad98a9e5e129de42a490
jeffreybarry <10522393+jeffreybarry@users.noreply.github.com>
2018-11-17 09:52:41 -0500

    Update page-soon.php
    Removed topbanner class in order to hide bottom border color, which is not needed on page-soon.

--- page-soon.php
+++ page-soon.php
@@ -11,9 +11,35 @@
  *
  * @package ShenAleph
  */
+ ?>
 
-get_header();
-?>
+ <!doctype html>
+ <html <?php language_attributes(); ?>>
+ <head>
+ 	<meta charset="<?php bloginfo( 'charset' ); ?>">
+ 	<meta name="viewport" content="width=device-width, initial-scale=1">
+ 	<link rel="profile" href="https://gmpg.org/xfn/11">
+ 	<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.jpg" />
+ <link rel="stylesheet" href="https://use.typekit.net/nci7nxi.css">
+ 	<?php wp_head(); ?>
+ </head>
+
+ <body <?php body_class(); ?>>
+ <!-- start Bootstrap container -->
+ <div id="page" class="site">
+	 <!-- removed topbanner class on section in order to remove border bottom color -->
+ 			<section class="container-fluid" id="typelogo-container">
+ 				<div class="row no-gutters">
+ 			<div class="col-md-3 typelogo-column">
+ 			<?php	the_custom_logo(); ?>
+ 		</div>
+ 		<div class="col-md-8">
+ 			<h1 class="text-right" id="typelogo">Shenandoah</h1>
+ 			</div>
+ 		</div>
+ 		</section>
+
+
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main container">
