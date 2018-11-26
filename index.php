35f617b71231f9615e6cb3245a34c7085462142e
jeffreybarry <10522393+jeffreybarry@users.noreply.github.com>
2018-11-26 15:14:24 -0500

    Update index.php

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 get_header();
 ?>
 <div class="row volumeIssue">
-<span class="mx-auto align-middle">Volume 68, xNumber 1 &middot; Fall 2018</span>
+<span class="mx-auto align-middle">Volume 68, Number 1 &middot; Fall 2018</span>
 </div>
 </section>
 	<div id="primary" class="content-area">
