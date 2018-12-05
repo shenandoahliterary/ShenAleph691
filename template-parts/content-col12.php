15b5b231cced1ab8e5d49a010e4000ab2d90e93a
jeffreybarry <10522393+jeffreybarry@users.noreply.github.com>
2018-12-05 17:10:28 -0500

    Update template-parts/content-col12.php

--- template-parts/content-col12.php
+++ template-parts/content-col12.php
@@ -9,7 +9,7 @@
 
 ?>
 <div class="row">
-<div class="col-md-8 offset-md-2">
+<div class="col-md-12">
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<?php
