1b86a491567579f6886788d457e8ac4b4225d646
jeffreybarry <10522393+jeffreybarry@users.noreply.github.com>
2018-12-06 11:38:11 -0500

    Update js/toggle-erasure.js

--- js/toggle-erasure.js
+++ js/toggle-erasure.js
@@ -2,11 +2,15 @@
 //$(document).ready(function () {
   console.log("in script");
   $('#translation-control-panel').css('visibility', 'visible');
+  $('#unerased').css('visibility', 'visible');
+  $('#erased').css('visibility', 'hidden');
 
     $("#select-unerased").click(function (e) {
       e.preventDefault();
       console.log("original clicked");
+      $('#erased').hide('slow');
       $('#unerased').hide('slow');
+
     //  $('#translation').css('visibility', 'hidden');
     //  $("#original").css('visibility', 'visible');
     //  if ($('#original').css('display') == 'none') {
@@ -17,7 +21,7 @@
     $("#select-erased").click(function (f) {
       f.preventDefault();
       console.log("erasure clicked");
-      $('#original').hide('slow');
+      $('#unerased').hide('slow');
       $('#erased').show('slow');
 
     });
