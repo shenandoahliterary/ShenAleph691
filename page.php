a6aaf623a967f97a31fcd7fe00fea992338564dd
jeffreybarry <10522393+jeffreybarry@users.noreply.github.com>
2018-12-07 06:13:16 -0500

    Update page.php

--- page.php
+++ page.php
@@ -22,38 +22,12 @@ get_header();
 
 			<?php
 			if ( have_posts() ) :
-				 
+
 				/* Start the Loop */
 				while ( have_posts() ) :
 					the_post();
-					/*
-					 * Include the Post-Type-specific template for the content.
-					 * If you want to override this in a child theme, then include a file
-					 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-					 */
-					 if ( has_tag('columns-3') ) :
-						 get_template_part( 'template-parts/content', 'col3' );
-
-						elseif ( has_tag('columns-12') ) :
-							 get_template_part( 'template-parts/content', 'col12' );
-
-					 elseif (has_tag('erasure') )  :
-	 									get_template_part( 'template-parts/content', 'erasure' );
-
-					elseif (has_tag('translation') )  :
-							get_template_part( 'template-parts/content', 'translation' );
 
 
-
-					 else :
-
-					get_template_part( 'template-parts/content', get_post_type() );
-				endif;
-		  	endwhile;
-				the_posts_navigation();
-			else :
-				get_template_part( 'template-parts/content', 'none' );
-			endif;
 			?>
 
 
