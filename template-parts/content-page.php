a83de8514a9508da58c54810f4df99ac3c4ae665
jeffreybarry <10522393+jeffreybarry@users.noreply.github.com>
2018-12-07 06:31:22 -0500

    Update template-parts/content-page.php

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -10,17 +10,13 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	
-	<?php shenAleph_post_thumbnail(); ?>
+
 
 	<div class="entry-content">
 		<?php
 		the_content();
 
-		wp_link_pages( array(
-			'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'shenAleph' ),
-			'after'  => '</div>',
-		) );
+
 		?>
 	</div><!-- .entry-content -->
 
