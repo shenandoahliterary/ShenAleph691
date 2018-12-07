d4425cf4d38917a3e2f8037123c7919846100501
jeffreybarry <10522393+jeffreybarry@users.noreply.github.com>
2018-12-07 08:25:19 -0500

    Update page-issues.php

--- page-issues.php
+++ page-issues.php
@@ -18,7 +18,7 @@ get_header();
 
 			<section class="container page-issues">
 			<div class="row">
-				<div class="col-md-8 offset-md-2">
+				<div class="col-md-9 offset-md-2">
 					<h1 class="entry-header entry-title">Issues</h1>
 <div class="card-deck">
 	<div class="card w-300"><a href="https://shenandoahliterary.org/682">
@@ -48,7 +48,8 @@ get_header();
 
 
 <div class="row">
-	<div class="col-md-8 offset-md-2">
+	<div class="col-md-9 offset-md-2">
+		<div class="card-deck">
 
 
 						<div class="card w-300"><a href="https://shenandoahliterary.org/671">
@@ -72,11 +73,12 @@ get_header();
 					    </div>
 						</a>
 					  </div>
-
+ </div>
 </div>
 </div>
 <div class="row">
-	<div class="col-md-8 offset-md-2">
+	<div class="col-md-9 offset-md-2">
+		<div class="card-deck">
 
 						<div class="card w-300"><a href="https://shenandoahliterary.org/652">
 					    <img class="card-img-top" src="https://shenandoahliterary.org/681/files/2018/12/issue621.jpg" alt="Card image cap">
@@ -99,11 +101,12 @@ get_header();
 					    </div>
 						</a>
 					  </div>
-
+ </div>
 </div>
 </div>
 <div class="row">
 	<div class="col-md-8 offset-md-2">
+		<div class="card-deck">
 
 						<div class="card"><a href="https://shenandoahliterary.org/641">
 					    <img class="card-img-top" src="https://shenandoahliterary.org/681/files/2018/12/issue621.jpg" alt="Card image cap">
@@ -126,11 +129,12 @@ get_header();
 					    </div>
 						</a>
 					  </div>
-
+  </div>
 </div>
 </div>
 <div class="row">
 	<div class="col-md-8 offset-md-2">
+		<div class="card-deck">
 
 						<div class="card"><a href="https://shenandoahliterary.org/622">
 					    <img class="card-img-top" src="https://shenandoahliterary.org/681/files/2018/12/issue621.jpg" alt="Card image cap">
@@ -160,7 +164,7 @@ get_header();
 					    </div>
 						</a>
 					  </div>
-
+</div>
 
 					</div>
 				</div>
