886da18af5dd27a3aae9e6d4c391a4ce538e4fa7
jeffreybarry <10522393+jeffreybarry@users.noreply.github.com>
2018-12-07 06:15:15 -0500

    Update page.php

--- page.php
+++ page.php
@@ -27,8 +27,64 @@ get_header();
 				while ( have_posts() ) :
 					the_post();
 
+					<div class="row">
+					<div class="col-md-8 offset-md-2">
+					<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+						<header class="entry-header">
+							<?php
+							if ( is_singular() ) :
+								the_title( '<h1 class="entry-title">', '</h1>' );
+							else :
+								the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+							endif;
 
-			?>
+							if ( 'post' === get_post_type() ) :
+								?>
+								<div class="entry-meta">
+									<!-- add byline -->
+									<p class="workAuthorByline"><?php
+					/* should add to filter: if filter is empty then only the_author_meta; if filter is not empty, then all authors from filter */
+								 the_author_meta('display_name');
+								 echo "<br />";
+								 shenAleph_filter_authors();
+
+								  ?></p>
+								</div><!-- .entry-meta -->
+							<?php endif; ?>
+						</header><!-- .entry-header -->
+
+						<?php shenAleph_post_thumbnail(); ?>
+
+						<div class="entry-content">
+							<?php
+							the_content( sprintf(
+								wp_kses(
+									/* translators: %s: Name of current post. Only visible to screen readers */
+									__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'shenAleph' ),
+									array(
+										'span' => array(
+											'class' => array(),
+										),
+									)
+								),
+								get_the_title()
+							) );
+
+							wp_link_pages( array(
+								'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'shenAleph' ),
+								'after'  => '</div>',
+							) );
+							?>
+						</div><!-- .entry-content -->
+
+	
+						<footer class="entry-footer">
+							<?php shenAleph_entry_footer(); ?>
+						</footer><!-- .entry-footer -->
+					</article><!-- #post-<?php the_ID(); ?> -->
+					</div>
+					</div>
+
 
 
 		</main><!-- #main -->
