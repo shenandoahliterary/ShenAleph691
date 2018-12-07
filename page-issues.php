be437c17906e932296a5d90ee20ef07a17b9b43a
jeffreybarry <10522393+jeffreybarry@users.noreply.github.com>
2018-12-07 08:29:33 -0500

    Update page-issues.php

--- page-issues.php
+++ page-issues.php
@@ -133,7 +133,7 @@ get_header();
 </div>
 </div>
 <div class="row">
-	<div class="col-md-9 offset-md-2">
+	<div class="col-md-10 offset-md-1">
 		<div class="card-deck">
 
 						<div class="card"><a href="https://shenandoahliterary.org/622">
