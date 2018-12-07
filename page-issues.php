9ea6786ae3b6b6d2a4353373a29435b39f9dafcd
jeffreybarry <10522393+jeffreybarry@users.noreply.github.com>
2018-12-07 08:04:18 -0500

    Update page-issues.php

--- page-issues.php
+++ page-issues.php
@@ -21,7 +21,7 @@ get_header();
 				<div class="col-md-8 offset-md-2">
 					<h1 class="entry-header entry-title">Issues</h1>
 
-					<div class="card-deck">
+					
 					  <div class="card"><a href="https://shenandoahliterary.org/621">
 					    <img class="card-img-top" src="https://shenandoahliterary.org/681/files/2018/12/issue621.jpg" alt="Card image cap">
 					    <div class="card-body">
@@ -127,7 +127,7 @@ get_header();
 					    </div>
 						</a>
 					  </div>
-					</div>
+
 
 
 					</div>
