d360f0a9c26c69e8321dc3957c822264d9179c2b
jeffreybarry <10522393+jeffreybarry@users.noreply.github.com>
2018-12-07 07:06:13 -0500

    Update template-parts/content-erasure.php

--- template-parts/content-erasure.php
+++ template-parts/content-erasure.php
@@ -10,7 +10,7 @@
 ?>
 <div class=""="row">
  <div id="erasure-control-panel" class="col-md-10 offset-md-2">
-<p class="text-right"><a href="#" id="select-erased"><i class="fas fa-toggle-on"></i> show erased text</a> | <a href="" id="select-unerased">show unerased text</a> </p>
+<p class="text-right"><a href="#" id="select-erased"><i class="far fa-eye-slash"></i> show erased text</a> | <a href="" id="select-unerased">show unerased text</a> </p>
 </div>
 </div>
 
