3cb0627d85b3591795d700b35a91525673b4d20a
jeffreybarry <10522393+jeffreybarry@users.noreply.github.com>
2018-12-14 14:08:51 -0500

    Update page-issues.php

--- page-issues.php
+++ page-issues.php
@@ -139,8 +139,6 @@ get_header();
 <div class="row">
 	<div class="col-md-10 offset-md-1">
 		<div class="card-deck">
-
-
 						<div class="card"><a href="https://shenandoahliterary.org/621">
 					    <img class="card-img-top" src="https://shenandoahliterary.org/681/files/2018/12/62.1.jpg" alt="Card image cap">
 					    <div class="card-body">
@@ -155,9 +153,6 @@ get_header();
 					    </div>
 						</a>
 					  </div>
-						<div class="row">
-							<div class="col-md-10 offset-md-1">
-								<div class="card-deck">
 									<div class="card"><a href="https://shenandoahliterary.org/611">
 										<img class="card-img-top" src="https://shenandoahliterary.org/681/files/2018/12/61.1.jpg" alt="Card image cap">
 										<div class="card-body">
@@ -165,12 +160,6 @@ get_header();
 										</div>
 									</a>
 									</div>
-
-</div>
-
-					</div>
-				</div>
-
 
 </div>
 </div>
