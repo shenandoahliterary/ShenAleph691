b832b26d7118167f67b71398fce6d5369c0489ef
jeffreybarry <10522393+jeffreybarry@users.noreply.github.com>
2019-05-09 21:18:18 -0400

    Update content-toc.php

--- template-parts/content-toc.php
+++ template-parts/content-toc.php
@@ -11,7 +11,7 @@ $uploads = wp_upload_dir();
 $upload_path =  $uploads['baseurl'];
 /* need to set image in admin dashboard */
 	?>
-<img  class="img-fluid cover-image w-100" src="<?php echo $upload_path ?>/2019/04/Impunidad_Circulo_Vicioso-640.jpg">
+<img  class="img-fluid cover-image w-100" src="<?php echo $upload_path ?>/2019/05/Impunidad_Circulo_Vicioso-640-height.jpg">
 </div>
 <div class="row volumeIssue">
 </div>
