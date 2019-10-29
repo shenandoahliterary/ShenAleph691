4273ea59d10a8864da2dc0701c434143b7f4fad1
jeffreybarry <iamjeffbarry@gmail.com>
2019-10-29 09:53:23 -0400

    Update page-issues.php

--- page-issues.php
+++ page-issues.php
@@ -34,7 +34,7 @@ get_header();
 	<div class="col-md-10 offset-md-1">
 		<div class="card-deck">
 			<div class="card w-300"><a href="https://shenandoahliterary.org/682">
-				<img class="card-img-top" src="https://shenandoahliterary.org/681/files/2018/12/681.-300x198.jpg" alt="Card image cap">
+				<img class="card-img-top" src="https://shenandoahliterary.org/682/files/2019/10/Impunidad_300-198.jpg" alt="Card image cap">
 				<div class="card-body">
 					<h5 class="card-title">Volume 68 Issue 2</h5>
 				</div>
