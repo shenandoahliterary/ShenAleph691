e9e93608e0f71b272fdd8eba67b04c65b41c23a0
jeffreybarry <iamjeffbarry@gmail.com>
2019-12-05 16:50:38 -0500

    footer updated with links

--- footer-bak.php
+++ footer-bak.php
@@ -18,12 +18,12 @@
 		<div class="site-info row">
 			<div class="col-md-12">
 				<p class="text-right"><a href="https://www.facebook.com/ShenandoahLiterary"><i class="fab fa-facebook-square"></i></a> <a href="https://www.instagram.com/shenandoah_literary"><i class="fab fa-instagram"></i></a> <a href="https://twitter.com/ShenandoahWLU"><i class="fab fa-twitter"></i></a>   </p>
-<a href = "https://shenandoahliterary.org/"><img class="img-fluid float-left shen-logo-footer" src="https://shenandoahliterary.org/681/files/2018/12/shen-w175.png"></a><a href = "https://www.wlu.edu/"><img class="img-fluid wlu-logo" src="https://shenandoahliterary.org/681/files/2018/12/wlu-w300.png"></a>
-<a href = "https://www.clmp.org/"><img class="img-fluid float-right" src="https://shenandoahliterary.org/681/files/2018/12/clmp.png" href = "https://www.clmp.org/"></a>
+<img class="img-fluid float-left shen-logo-footer" src="https://shenandoahliterary.org/681/files/2018/12/shen-w175.png"><img class="img-fluid wlu-logo" src="https://shenandoahliterary.org/681/files/2018/12/wlu-w300.png">
+<img class="img-fluid float-right" src="https://shenandoahliterary.org/681/files/2018/12/clmp.png">
 			</div>
 <div class="row">
 	<div class="col-md-12">
-	<p class="copyright">&copy; 2019 Shenandoah</p>
+	<p class="copyright">&copy; 2018-2019 Shenandoah</p>
 </div>
 
 		</div><!-- .site-info -->
